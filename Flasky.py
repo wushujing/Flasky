51850428680770c655ee167d42a6c846751dd65e
wushujing <wushujing@outlook.com>
2015-11-01 19:24:32 +0800

    404 Page extends base Template

--- Flasky.py
+++ Flasky.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -13,6 +15,10 @@ def user(name):
     strr = '<h1>Test Safe</h1>'
     return render_template('user.html', name=name, strr=strr)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
